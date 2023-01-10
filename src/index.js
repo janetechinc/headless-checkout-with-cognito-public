164e9cc74bf22e9b3a245223a38835da60b1b142
Dana Levine <dana@iheartjane.com>
2023-01-09 18:23:41 -0800

    comment out web vitals

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import config from './config';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 Amplify.configure({
@@ -36,4 +36,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
